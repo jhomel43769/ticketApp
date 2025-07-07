db5507a2f11d8bc6fa3f707ada41ad581ea0e80c
Jhomel Medina <jhomelmedina2@gmail.com>
2025-07-07 04:06:42 -0400

    - roles model termiando

---
+++ src/models/roles.mode.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const roleSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    createdAtL: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    Permissions: [{
+        type: String,
+        enum: [
+        'project:create', 'project:read', 'project:update', 'project:delete',
+        'issue:create', 'issue:read', 'issue:update', 'issue:delete',
+        'user:read_all', 'user:manage_roles'    
+        ]
+    }]
+})
+
+roleSchema.pre('save', function(next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+export const Role = mongoose.model('Role', roleSchema);
+
